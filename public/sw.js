d52e8b4273026ae2a3871a412ca71044d4e3d2e2
Kansas Nelly <kansasiinelly@gmail.com>
2026-05-28 17:06:29 +0700

    fix: prevent service worker caching non-GET requests to avoid Cache Storage errors

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,9 @@ self.addEventListener('fetch', (event) => {
   if (!event.request.url.startsWith('http')) {
     return;
   }
+  if (event.request.method !== 'GET') {
+    return; // Do not attempt to cache POST, HEAD, PUT, or DELETE requests
+  }
   event.respondWith(
     caches.open('earnings-cache').then((cache) => {
       return cache.match(event.request).then((response) => {

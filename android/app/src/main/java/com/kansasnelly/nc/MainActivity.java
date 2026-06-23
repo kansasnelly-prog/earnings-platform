0a9136fe29bf25863ad2778224caa694bc6fca3e
Kansas Nelly <kansasiinelly@gmail.com>
2026-06-24 06:59:56 +0700

    feat: commit native Android platform project architecture and capacitor config files

---
+++ android/app/src/main/java/com/kansasnelly/nc/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kansasnelly.nc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kansasnelly.nc',
+  appName: 'Nelly Coins',
+  webDir: 'dist'
+};
+
+export default config;

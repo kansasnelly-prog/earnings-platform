2170e704cb964f1dbc5fe6782280850ad1cde8aa
Kansas Nelly <kansasiinelly@gmail.com>
2026-08-16 09:29:57 +0700

    feat: initialize Capacitor Android build and resolve Electron ES module error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.kansasnelly.nc',
-  appName: 'Nelly Coins',
-  webDir: 'dist'
+  appId: 'com.sreymara.app',
+  appName: 'SREYMARA',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
